6b3aac3ff8ea17dc10a78c05e5c1ad5cc593de2a
master <master@SMLAB408PC23_23>
2023-01-24 19:29:00 -0500

    Agregar archivos de proyecto.

---
+++ ivegaS7/dataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ivegaS7
+{
+    public interface dataBase
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
